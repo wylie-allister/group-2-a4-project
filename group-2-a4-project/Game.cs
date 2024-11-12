5a0d3311109d567ea3166e792e49c0df4d00ceec
Allister Wylie <000840304@mohawkcollege.ca>
2024-11-12 16:57:02 -0500

    Add 2D Game template

---
+++ group-2-a4-project/Game.cs
@@ -0,0 +1,32 @@
+﻿// Include code libraries you need below (use the namespace).
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace Game10003
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+}
