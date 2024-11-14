31af8ebe3d59c6aa5e8269b517ebb25e23ea398e
wylie-allister <000840304@mohawkcollege.ca>
2024-11-13 23:19:43 -0500

    Add wall parameters

--- group-2-a4-project/PlayerCollision.cs
+++ group-2-a4-project/PlayerCollision.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Numerics;
 
-namespace Game10003
-{
-    public class PlayerCollision
-    {
+namespace Game10003;
 
+public class PlayerCollision
+{
+    public void Wall()
+    {
+        //Creates wall collision
+        //Variable names may be incorrect, will plug in player class when created
+        float playerLeftEdge = playerPosition.X;
+        float playerRightEdge = playerPosition.X + playerSize.X;
+        float playerTopEdge = playerPosition.Y;
+        float playerBottomEdge = playerPosition.Y + playerSize.Y;
+
+        //Defines window parameters, right and left can be defined if needed, but are unneccessary for right now 
+        bool topOfWindow = playerTopEdge <= 0;
+        bool bottomOfWindow = playerBottomEdge >= Window.Height;
+
+        //Insert definitions for what happens here (most likely --health or kill screen, that's not on me though)
+        
+    }
+
+    public void ObstacleCollision()
+    {
+        //it is 11pm and i do not have the mental fortitude to continue working on this right now, i was supposed to have a nice relaxing wednesday ;-;
     }
 }
