b06f2f35fbcfe5b606f88edf4e9fbce5e79eef3a
Austin Rebelo <000917638@mohawkcollege.ca>
2024-11-19 15:55:35 -0500

    Add functions that load audio, Add function that play audio

--- group-2-a4-project/Game.cs
+++ group-2-a4-project/Game.cs
@@ -1,6 +1,7 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
 using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
 
 // The namespace your code is in.
 namespace Game10003
@@ -11,14 +12,17 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        GameAudio test = new GameAudio();
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("test");
+            Window.SetSize(400, 400);
+            test.LoadAllAudio();
+            test.PlayMusic();
         }
 
         /// <summary>
@@ -26,7 +30,15 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.G))
+            {
+                test.PlayGravity();
+            }
 
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.B))
+            {
+                test.PlayBoom();
+            }
         }
     }
 }
---
+++ group-2-a4-project/GameAudio.cs
@@ -0,0 +1,38 @@
+﻿using Raylib_cs;
+using System;
+using System.Numerics;
+
+namespace Game10003;
+
+public class GameAudio
+{
+    Sound boomSound;
+    Sound gravitySound;
+    Music[] backgroundMusic = new Music[3];
+
+    public void LoadAllAudio()
+    {
+        backgroundMusic[0] = Audio.LoadMusic("../../../Assets/Audio/music-meatball.mp3");
+        backgroundMusic[1] = Audio.LoadMusic("../../../Assets/Audio/music-run.mp3");
+        backgroundMusic[2] = Audio.LoadMusic("../../../Assets/Audio/music-upbeat.mp3");
+
+        boomSound = Audio.LoadSound("../../../Assets/Audio/kaboom.wav");
+        gravitySound = Audio.LoadSound("../../../Assets/Audio/jump.mp3");
+    }
+
+    public void PlayMusic()
+    {
+        int randomMusic = Random.Integer(0, backgroundMusic.Length);
+        Audio.Play(backgroundMusic[randomMusic]);
+    }
+
+    public void PlayBoom()
+    {
+        Audio.Play(boomSound);
+    }
+
+    public void PlayGravity()
+    {
+        Audio.Play(gravitySound);
+    }
+}
