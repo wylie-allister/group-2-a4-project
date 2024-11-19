f77127cf0d790be111274b24761ef9c023e6e956
Austin Rebelo <000917638@mohawkcollege.ca>
2024-11-19 15:58:16 -0500

    Add comments

--- group-2-a4-project/GameAudio.cs
+++ group-2-a4-project/GameAudio.cs
@@ -10,6 +10,9 @@ public class GameAudio
     Sound gravitySound;
     Music[] backgroundMusic = new Music[3];
 
+    /// <summary>
+    ///     Loads all audio files
+    /// </summary>
     public void LoadAllAudio()
     {
         backgroundMusic[0] = Audio.LoadMusic("../../../Assets/Audio/music-meatball.mp3");
@@ -20,17 +23,26 @@ public class GameAudio
         gravitySound = Audio.LoadSound("../../../Assets/Audio/jump.mp3");
     }
 
+    /// <summary>
+    ///     Plays random song from array
+    /// </summary>
     public void PlayMusic()
     {
         int randomMusic = Random.Integer(0, backgroundMusic.Length);
         Audio.Play(backgroundMusic[randomMusic]);
     }
 
+    /// <summary>
+    ///     Plays kaboom sound
+    /// </summary>
     public void PlayBoom()
     {
         Audio.Play(boomSound);
     }
 
+    /// <summary>
+    ///     Plays gravity sound
+    /// </summary>
     public void PlayGravity()
     {
         Audio.Play(gravitySound);
