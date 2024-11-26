289174b926cabb363ccbdde946c889fa342d4fa6
Owen Miller Johnson <000937901@mohawkcollege.ca>
2024-11-26 15:52:07 -0500

    Player class

---
+++ group-2-a4-project/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace group_2_a4_project
+{
+    public class Player()
+    {
+        Vector2 playerPosition = new Vector2(150, 300);
+        Vector2 PlayerSize = new Vector2(20, 20);
+
+
+    }
+}
