45bf0b2451ee9b3402d494f42340ef2771703a19
Owen Miller Johnson <000937901@mohawkcollege.ca>
2024-11-30 13:45:55 -0500

    Class

--- group-2-a4-project/Game.cs
+++ group-2-a4-project/Game.cs
@@ -10,17 +10,18 @@ namespace Game10003
     /// </summary>
     public class Game
     {
-        // Place your variables here:
+        // Player size and starting position 
 
         Vector2 playerPosition = new Vector2(150, 300);
         Vector2 PlayerSize = new Vector2(20, 20);
 
-        
+
 
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
+        /// The window size
         public void Setup()
         {
             Window.SetSize(600, 600);
@@ -29,6 +30,7 @@ namespace Game10003
         /// <summary>
         ///     Update runs every frame.
         /// </summary>
+        /// Colour of the player character.
         public void Update()
         {
             Draw.FillColor = Color.White;
--- group-2-a4-project/Player.cs
+++ group-2-a4-project/Player.cs
@@ -3,11 +3,11 @@ using System.Numerics;
 
 namespace group_2_a4_project
 {
-    public class Player()
+    public class Player
     {
         Vector2 playerPosition = new Vector2(150, 300);
         Vector2 PlayerSize = new Vector2(20, 20);
-
-
     }
+    
+    
 }
